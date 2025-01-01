b75e4d60d815d02c5495b57ba9df556b012cd172
HishamSalem <63689667+HishamSalem@users.noreply.github.com>
2025-01-01 15:53:00 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pymltools",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'xgboost',
+        'shap',
+        'optbinning',
+        'SALib',
+        'scipy',
+        'statsmodels',
+        'tqdm',
+        'matplotlib',
+        'seaborn',
+        'joblib'
+    ],
+    author="Hisham Salem",
+    author_email="hisham.salem@mail.mcgill.ca",
+    description="A package for analyzing feature interactions in machine learning models",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/feature_interaction",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
