f56a4a75083016a0d752f115d2ffb49b4d22bef0
HishamSalem <63689667+HishamSalem@users.noreply.github.com>
2026-03-16 19:04:55 -0400

    Refactor __init__.py for version update and API changes
    Updated version to 0.1.0 and added new classes to public API.

--- predykt/__init__.py
+++ predykt/__init__.py
@@ -1,11 +1,15 @@
-from .feature_binning import *
-from .shap_significance import *
+from .feature_binning import FeatureBinningAnalyzer
+from .cyclical_transformer import CyclicalBinner
+from .interaction_stability import InteractionTester, InteractionVoter
+from .seed_robustness import SeedRobustnessValidator
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "Hisham Salem"
 
-# Define public API
 __all__ = [
-    'feature_binning',
-    'shap_significance'
+    "FeatureBinningAnalyzer",
+    "CyclicalBinner", 
+    "InteractionTester",
+    "InteractionVoter",
+    "SeedRobustnessValidator",
 ]
